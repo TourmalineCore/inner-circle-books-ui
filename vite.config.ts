e92b842611ccd3c4be28e04adb172b2798e31e31
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-03-20 11:17:10 +0500

    chore: refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,18 +8,20 @@ import svgr from 'vite-plugin-svgr'
 const LOCAL_ENV_PORT = 40100
 const BOOKS_PORT = process.env.NODE_ENV === `production` ? LOCAL_ENV_PORT : 4005
 
-// for run in local docker use `http://localhost:4455/assets..`, and for others use `../layout/assets..` path
-const LAYOUT_PATH = process.env.VITE_BASE_URL === `http://localhost:4455` ? `` : `/layout`
+// for run in local docker use `/`, and for others use `/books` path
+const BOOKS_PATH = process.env.VITE_BASE_URL === `http://localhost:4457` ? `/` : `/books`
 
-// const LAYOUT_PORT = process.env.NODE_ENV === `production` ? LOCAL_ENV_PORT : 4006
+// for run in local docker use `http://localhost:4455/assets..`, and for others use `../layout/assets..` path
+const LAYOUT_PATH = process.env.VITE_BASE_URL === `http://localhost:4457` ? `` : `/layout`
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   server: {
     port: BOOKS_PORT,
   },
-  // base: `/`, // for local docker
-  base: `/books`, // for local-env
+  // `/` for local docker
+  // `/books` for local-env and prod
+  base: BOOKS_PATH,
   plugins: [
     react(),
     svgr(),
@@ -29,9 +31,9 @@ export default defineConfig({
       filename: "inner_circle_books_ui.js",
       // The path where the remote application file can be found and its name
       remotes: {
-        // inner_circle_layout_ui: `http://localhost:4455/assets/inner_circle_layout_ui.js`, // for local docker
-        // inner_circle_layout_ui: `http://localhost:40100/layout/assets/inner_circle_layout_ui.js`, // for local-env
-        inner_circle_layout_ui: `${process.env.VITE_BASE_URL}${LAYOUT_PATH}/assets/inner_circle_layout_ui.js`, // for prod
+        // `http://localhost:4457/assets/inner_circle_layout_ui.js` for local docker
+        // `http://localhost:40100/layout/assets/inner_circle_layout_ui.js` for local-env
+        inner_circle_layout_ui: `${process.env.VITE_BASE_URL}${LAYOUT_PATH}/assets/inner_circle_layout_ui.js`,
       },
       shared: [
         "react",
