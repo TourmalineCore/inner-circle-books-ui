a8c229e8a45b4ab1f304b561a1de9f15f9179835
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-03-20 15:37:55 +0500

    chore: remove local docker run for now

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,13 @@ const LOCAL_ENV_PORT = 40100
 const BOOKS_PORT = process.env.NODE_ENV === `production` ? LOCAL_ENV_PORT : 4005
 
 // if (VITE_BASE_URL === undefined) set default local docker url
-const LAYOUT_URL = process.env.VITE_BASE_URL ?? `http://localhost:4455`
+// const LAYOUT_URL = process.env.VITE_BASE_URL ?? `http://localhost:4455`
 
 // for run in local docker use `/`, and for others use `/books` path
-const BOOKS_PATH = LAYOUT_URL === `http://localhost:4455` ? `/` : `/books`
+// const BOOKS_PATH = LAYOUT_URL === `http://localhost:4455` ? `/` : `/books`
 
 // for run in local docker use `http://localhost:4455/assets..`, and for others use `../layout/assets..` path
-const LAYOUT_PATH = LAYOUT_URL === `http://localhost:4455` ? `` : `/layout`
+// const LAYOUT_PATH = LAYOUT_URL === `http://localhost:4455` ? `` : `/layout`
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   // `/` for local docker
   // `/books` for local-env and prod
-  base: BOOKS_PATH,
+  base: `/books`,
   plugins: [
     react(),
     svgr(),
@@ -36,7 +36,7 @@ export default defineConfig({
       remotes: {
         // `http://localhost:4455/assets/inner_circle_layout_ui.js` for local docker
         // `http://localhost:40100/layout/assets/inner_circle_layout_ui.js` for local-env
-        inner_circle_layout_ui: `${LAYOUT_URL}${LAYOUT_PATH}/assets/inner_circle_layout_ui.js`,
+        inner_circle_layout_ui: `${process.env.VITE_BASE_URL}/layout/assets/inner_circle_layout_ui.js`,
       },
       shared: [
         "react",
@@ -45,7 +45,7 @@ export default defineConfig({
   ],
   define: {
     'import.meta.env.VITE_BASE_PATH': JSON.stringify(
-      LAYOUT_URL === `http://localhost:4455` ? `` : `/books`,
+      process.env.NODE_ENV === `production` ? `/books` : ``,
     ),
     define: {
       'import.meta.env.VITE_BASE_URL': process.env.VITE_BASE_URL,
