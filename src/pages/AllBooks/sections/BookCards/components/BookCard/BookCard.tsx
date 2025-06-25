2eb8f08f3385f57e3b673f7e940fb238b13f1cc4
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-06-25 13:52:00 +0500

    feat: add book card

---
+++ src/pages/AllBooks/sections/BookCards/components/BookCard/BookCard.tsx
@@ -0,0 +1,34 @@
+import { observer } from "mobx-react-lite"
+import './BooksCard.scss'
+
+export const BookCard = observer(({
+  bookCoverUrl,
+  title,
+  authors,
+  language,
+}: BookCardType) => {
+  const firstAuthor = authors[0]?.fullName
+  const suffix = authors.length > 1 ? (language === `rus` ? `и др.` : `and etc.`) : ``
+
+  return (
+    <div
+      className="card"
+      data-cy="card"
+    >
+      <img
+        src={bookCoverUrl}
+        alt={title}
+        className="card__image"
+      />
+      <div className="card__about">
+        <div className="card__title">{title}</div>
+        <div className="card__wrap">
+          <div className="card__author">
+            {firstAuthor} {suffix}
+          </div>
+          <div className="card__language">{language}</div>
+        </div>
+      </div>
+    </div>
+  )
+})
