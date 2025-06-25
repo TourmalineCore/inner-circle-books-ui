63a066fa43b8c681a16718f6d3394fbf81818c79
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2025-06-25 13:56:10 +0500

    feat: add book cards state

---
+++ src/pages/AllBooks/sections/BookCards/state/BookCardsState.ts
@@ -0,0 +1,22 @@
+import { makeAutoObservable } from 'mobx'
+
+export class BookCardsState {
+  private _booksCards: BookCardType[] = []
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  initialize({
+    booksCards,
+  }: {
+    booksCards: BookCardType[],
+  }) {
+    this._booksCards = booksCards
+  }
+
+  get booksCards() {
+    return this._booksCards
+  }
+
+}
