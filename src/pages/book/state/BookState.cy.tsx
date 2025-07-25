2c1b744761f193a4c7fc9554a0928ded8bf14f39
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-07-25 15:38:56 +0500

    test: add state tests

--- src/pages/book/state/BookState.cy.tsx
+++ src/pages/book/state/BookState.cy.tsx
@@ -81,4 +81,94 @@ describe(`BookState`, () => {
   `, () => {
     expect(bookState.count).to.eq(2)
   })
+
+  it(`
+  GIVEN the BookState
+  WHEN toggleBookCopyChecked
+  SHOULD toggle the selected state of a book copy
+  `, () => {
+    expect(bookState.isBookCopySelected({
+      bookCopyId: 1, 
+    })).to.be.false
+
+    bookState.toggleBookCopyChecked({
+      bookCopyId: 1, 
+    })
+
+    expect(bookState.isBookCopySelected({
+      bookCopyId: 1, 
+    })).to.be.true
+
+    bookState.toggleBookCopyChecked({
+      bookCopyId: 1, 
+    })
+
+    expect(bookState.isBookCopySelected({
+      bookCopyId: 1, 
+    })).to.be.false
+  })
+
+  it(`
+  GIVEN the BookState
+  WHEN toggleSelectAllCopies with true
+  SHOULD select all book copies
+  `, () => {
+    bookState.toggleSelectAllCopies({
+      checked: true, 
+    })
+
+    expect(bookState.areAllCopiesSelected).to.be.true
+
+    expect(bookState.isBookCopySelected({
+      bookCopyId: 1, 
+    })).to.be.true
+
+    expect(bookState.isBookCopySelected({
+      bookCopyId: 2, 
+    })).to.be.true
+  })
+
+  it(`
+  GIVEN the BookState
+  WHEN toggleSelectAllCopies with false
+  SHOULD deselect all book copies
+  `, () => {
+    bookState.toggleSelectAllCopies({
+      checked: false, 
+    })
+
+    expect(bookState.areAllCopiesSelected).to.be.false
+
+    expect(bookState.isBookCopySelected({
+      bookCopyId: 1, 
+    })).to.be.false
+
+    expect(bookState.isBookCopySelected({
+      bookCopyId: 2, 
+    })).to.be.false
+  })
+
+  it(`
+  GIVEN the BookState
+  WHEN areAllCopiesSelected
+  SHOULD return true if all copies are selected
+  `, () => {
+    bookState.toggleSelectAllCopies({
+      checked: true, 
+    })
+
+    expect(bookState.areAllCopiesSelected).to.be.true
+  })
+
+  it(`
+  GIVEN the BookState
+  WHEN areAllCopiesSelected
+  SHOULD return false if not all copies are selected
+  `, () => {
+    bookState.toggleBookCopyChecked({
+      bookCopyId: 1, 
+    })
+    
+    expect(bookState.areAllCopiesSelected).to.be.false
+  })
 })
