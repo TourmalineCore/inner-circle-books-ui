efbd93f70c90d906b25b6a795605d92553f5d2aa
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-09-27 15:52:46 +0500

    test: separate books-smoke and take-return flow e2e

--- cypress/e2e/books-smoke.cy.ts
+++ cypress/e2e/books-smoke.cy.ts
@@ -1,6 +1,5 @@
 import { AllBooksPage } from "./pages/AllBooksPage"
 import { AddBookPage } from "./pages/AddBookPage"
-import { BookPage } from "./pages/BookPage"
 
 describe(`Books Smoke`, () => {
   beforeEach(`Authorize and cleanup`, () => {
@@ -18,60 +17,13 @@ describe(`Books Smoke`, () => {
   SHOULD see it in the books list on the AllBooks page
   AND click on it
   SHOULD see it's correct creation data on the Book page
-  AND take this book
-  SHOULD display our name as a new reader name on the Book page
-  AND return this book
-  SHOULD not display our name as a new reader name on the Book page
   `, () => {
     AllBooksPage.visit()
 
     AllBooksPage.checkNoBooks()
 
-    cy.intercept(
-      `POST`, 
-      `/api/books`)
-      .as(`addBookRequest`)
-
     AddBookPage.addBook()
 
-    cy.wait(`@addBookRequest`)
-      .then((interception) => {
-        const response = interception.response
-        const bookId = response!.body.newBookId
-       
-        cy.intercept(
-          `GET`, 
-          `/api/books/${bookId}`)
-          .as(`getBookDataRequest`)
-
-        AllBooksPage.checkAddedBook()
-
-        cy.wait(`@getBookDataRequest`)
-          .then((interception) => {
-            const response = interception.response
-            const bookCopyId = response!.body.bookCopiesIds[0]
-
-            BookPage.checkReadersBeforeTakeBook()
-
-            BookPage.visitCopy({
-              bookId,
-              bookCopyId,
-            })
-
-            BookPage.takeBook()
-
-            BookPage.visitCopy({
-              bookId,
-              bookCopyId,
-            })
-
-            BookPage.checkBookPageAfterTakeBook()
-            
-            BookPage.clickReturnBookButton()
-
-            // fix return book button action
-            // ReturnBookPage.returnBook()
-          })
-      })
+    AllBooksPage.checkAddedBook()
   })
 })
