61988bb4797918efa87ea3decdad32a52593b03f
Ekaterina <echekina@gmail.com>
2026-05-21 11:26:33 +0500

    feat: #72: fix a11y

--- src/pages/all-books/components/filter-mobile/components/filter-modal/FilterModal.tsx
+++ src/pages/all-books/components/filter-mobile/components/filter-modal/FilterModal.tsx
@@ -1,5 +1,6 @@
 import "./FilterModal.scss"
 import clsx from "clsx"
+import FocusLock from 'react-focus-lock'
 import { Button } from "../../../../../../components/button/Button"
 import ArrowLeftIcon from "../../../../../../assets/icons/ArrowLeft.svg?react"
 
@@ -25,71 +26,76 @@ export const FilterModal = ({
   onClose: () => unknown,
 }) => {
   return (
-    <div 
-      className="filter-modal"
-      data-cy="filter-modal"
+    <FocusLock
+      returnFocus
     >
-      <div className="filter-modal__header">
-        <button
-          type="button"
-          className="filter-modal__back"
-          onClick={() => {
-            onClose()
-            resetToPreviouslySelectedAreas()
-          }}
-        >
-          <ArrowLeftIcon />
-        </button>
+      <div 
+        className="filter-modal"
+        data-cy="filter-modal"
+      >
+        <div className="filter-modal__header">
+          <button
+            type="button"
+            className="filter-modal__back"
+            onClick={() => {
+              onClose()
+              resetToPreviouslySelectedAreas()
+            }}
+            aria-label="Exit filters modal window"
+          >
+            <ArrowLeftIcon />
+          </button>
 
-        <h2 className="filter-modal__title">
+          <h2 className="filter-modal__title">
           Filters
-        </h2>
-      </div>
+          </h2>
+        </div>
 
-      <div className="filter-modal__content">
-        <div className="filter-modal__section">
-          <h3 className="filter-modal__subtitle">
+        <div className="filter-modal__content">
+          <div className="filter-modal__section">
+            <h3 className="filter-modal__subtitle">
             Knowledge Areas
-          </h3>
+            </h3>
 
-          <div className="filter-modal__chips">
-            {knowledgeAreas.map(({
-              id, name,
-            }) => (
-              <button
-                key={id}
-                type="button"
-                className={clsx(`filter-modal__chip`, {
-                  "filter-modal__chip--active": selectedAreasIds.includes(id),
-                })}
-                onClick={() => toggleKnowledgeArea({
-                  knowledgeAreaId: id,
-                })}
-              >
-                {name}
-              </button>
-            ))}
+            <div className="filter-modal__chips">
+              {knowledgeAreas.map(({
+                id, name,
+              }) => (
+                <button
+                  key={id}
+                  type="button"
+                  className={clsx(`filter-modal__chip`, {
+                    "filter-modal__chip--active": selectedAreasIds.includes(id),
+                  })}
+                  onClick={() => toggleKnowledgeArea({
+                    knowledgeAreaId: id,
+                  })}
+                >
+                  {name}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="filter-modal__footer">
-        <Button
-          className="filter-modal__button"
-          onClick={resetFilters}
-          label={`Reset Filters`}
-        />
+        <div className="filter-modal__footer">
+          <Button
+            className="filter-modal__button"
+            onClick={resetFilters}
+            label={`Reset Filters`}
+          />
 
-        <Button
-          className="filter-modal__button"
-          onClick={() => {
-            onClose()
-            applySelectedAreas()
-          }}
-          label={`Apply`}
-          isAccent
-        />
+          <Button
+            className="filter-modal__button"
+            onClick={() => {
+              onClose()
+              applySelectedAreas()
+            }}
+            label={`Apply`}
+            isAccent
+          />
+        </div>
       </div>
-    </div>
+    </FocusLock>
   )
 }
